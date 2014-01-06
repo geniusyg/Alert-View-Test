ef61051b3e668ec0fc9c7539616a108f28d70b2a
Chae Yungi <l8673901@gmail.com>
2014-01-06 11:26:07 +0900

    Alert View Test
    Alert View Test

--- ExAlertView01/ViewController.m
+++ ExAlertView01/ViewController.m
@@ -14,10 +14,54 @@
 
 @implementation ViewController
 
+- (void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+	if(1==alertView.tag) {
+		if(alertView.cancelButtonIndex == buttonIndex) {
+			NSLog(@"취소");
+		} else if(alertView.firstOtherButtonIndex == buttonIndex) {
+			NSLog(@"선택1");
+		} else if(alertView.firstOtherButtonIndex+1 == buttonIndex) {
+			NSLog(@"선택2");
+		}
+	} else if(2==alertView.tag) {
+		if(alertView.firstOtherButtonIndex == buttonIndex) {
+			UITextField *idf = [alertView textFieldAtIndex:0];
+			UITextField *pwf = [alertView textFieldAtIndex:1];
+			NSLog(@"ID : %@ / PW : %@",idf.text, pwf.text);
+		} else {
+			NSLog(@"로그인 취소!");
+		}
+	} else {
+		NSLog(@"기타처리");
+	}
+}
+
+-(BOOL)alertViewSholdEnableFirstOtherButton:(UIAlertView *)alertView {
+	if(2==alertView.tag) {
+		UITextField *idf = [alertView textFieldAtIndex:0];
+		UITextField *pwf = [alertView textFieldAtIndex:1];
+		
+		return ([idf.text length]>3 && [pwf.text length]>3);
+	}
+	return YES;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)basicAlert:(id)sender {
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"제목" message:@"메시지" delegate:self cancelButtonTitle:@"취소" otherButtonTitles:@"확인", nil];
+	alert.tag = 1;
+	[alert show];
+}
+- (IBAction)alertWithInput:(id)sender {
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"로그인" message:@"아이디와 비밀번호를 입력해" delegate:self cancelButtonTitle:@"취소" otherButtonTitles:@"선택1",@"선택2", nil];
+	alert.alertViewStyle = UIAlertViewStyleLoginAndPasswordInput;
+	alert.tag = 2;
+	[alert show];
+	
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,3 +71,14 @@
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
